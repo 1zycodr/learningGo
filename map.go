4fae388133e166d536838b4c1961f2a4b485df66
mukhitdinov.a <5zamiggos@gmail.com>
2023-11-10 19:15:41 +0600

    slices structs

--- map.go
+++ map.go
@@ -1,5 +1,22 @@
 package main
 
-func Map() {
+import "fmt"
 
+func main() {
+	testMap := map[int]string{
+		1: "value 1",
+	}
+
+	testMap[2] = "value 2"
+
+	for k, v := range testMap {
+		fmt.Println(k, v)
+	}
+
+	delete(testMap, 1)
+
+	fmt.Println("After deletion")
+	for k, v := range testMap {
+		fmt.Println(k, v)
+	}
 }
