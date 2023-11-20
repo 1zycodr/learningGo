18361eba943c01a782670f37875d041bd88b9f70
mukhitdinov.a <5zamiggos@gmail.com>
2023-11-20 21:17:05 +0600

    waitgroups

---
+++ concurrency/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func printString(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(s)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	words := []string{
+		"a", "b", "c", "d", "e", "f",
+	}
+
+	for _, word := range words {
+		go printString(word, &wg)
+		wg.Add(1)
+	}
+
+	wg.Wait()
+}
